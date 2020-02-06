5e1dd4bf6077570d7aae8ca99a006cf573359cd3
vyoma <vyoma98@gmail.com>
2020-02-06 13:29:11 +0530

    Removed Incomplete ListSort

--- src/Components/InCompleteList.js
+++ src/Components/InCompleteList.js
@@ -1,53 +1,52 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import putInCompletedList from '../redux/actions/putToCompletedList';
 import { List, Checkbox } from 'antd';
-import ListSort from '../Animations/listsort'
+// import ListSort from '../Animations/listsort'
 
-class ConnectedInCompToDoList extends React.Component{
-    constructor(props){
-        super(props);
-      
-      this.checkBoxChange.bind(this);
-    }
-  
-  
-    checkBoxChange(key){
-      this.props.putItemToCompletedListByKey(key);
-      // this.props.updateList([...this.props.todoList.slice(0,index),...this.props.todoList.slice(index+1)]);
-    }
-    
-   render(){
-     console.log(this.props.todoList);
-     const childrenToRender = this.props.todoList.filter(item => !item.checked).map((item) => (
+class ConnectedInCompToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.checkBoxChange.bind(this);
+  }
+
+
+  checkBoxChange(key) {
+    this.props.putItemToCompletedListByKey(key);
+    // this.props.updateList([...this.props.todoList.slice(0,index),...this.props.todoList.slice(index+1)]);
+  }
+
+  render() {
+    console.log(this.props.todoList);
+    const childrenToRender = this.props.todoList.filter(item => !item.checked).map((item) => (
       <List.Item key={item.key}>
-        <Checkbox onChange={(event)=>this.checkBoxChange(item.key)}>{item.todos}</Checkbox>
+        <Checkbox onChange={(event) => this.checkBoxChange(item.key)}>{item.todos}</Checkbox>
       </List.Item>
     ));
-    if(childrenToRender.length)
-     return (
+
+    return childrenToRender.length === 0 ? null : (
       <List itemLayout="horizontal">
-        <ListSort>
+        {/* <ListSort> */}
           {childrenToRender}
-        </ListSort>
+        {/* </ListSort> */}
       </List>
-     );
-    return (<></>);
+    );
   }
-  }
+}
 
-  const mapStateToProps = (state) => {
-    return {
-      todoList: state
-    };
+const mapStateToProps = (state) => {
+  return {
+    todoList: state
   };
+};
 
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      putItemToCompletedListByKey: (key) => dispatch(putInCompletedList(key))
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    putItemToCompletedListByKey: (key) => dispatch(putInCompletedList(key))
   };
+};
 
-  const InCompleteToDoList = connect(mapStateToProps, mapDispatchToProps)(ConnectedInCompToDoList);
+const InCompleteToDoList = connect(mapStateToProps, mapDispatchToProps)(ConnectedInCompToDoList);
 
-  export default InCompleteToDoList;
+export default InCompleteToDoList;
